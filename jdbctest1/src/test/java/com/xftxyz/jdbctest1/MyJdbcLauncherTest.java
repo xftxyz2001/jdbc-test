9168c330ed185a708af07809b9244ae724742884
通晓宇宙 <2581011320@qq.com>
2022-05-17 23:58:42 +0800

    瞎尝试

---
+++ jdbctest1/src/test/java/com/xftxyz/jdbctest1/MyJdbcLauncherTest.java
@@ -0,0 +1,10 @@
+package com.xftxyz.jdbctest1;
+
+import org.junit.Test;
+
+public class MyJdbcLauncherTest {
+    @Test
+    public void testInjector(){
+        new MyJdbcLauncher().testInjector();
+    }
+}
---
+++ jdbctest2/src/main/java/com/xftxyz/jdbctest2/ICompanyDao.java
@@ -0,0 +1,21 @@
+package com.xftxyz.jdbctest2;
+
+import com.googlecode.jdbcproc.daofactory.annotation.AStoredProcedure;
+
+import java.util.List;
+import java.util.Iterator;
+
+/**
+ * Test dao
+ */
+public interface ICompanyDao {
+
+    /**
+     * Creates company
+     *
+     * @param aCompany new company
+     */
+    @AStoredProcedure(name = "create_company")
+    void createCompany(Company aCompany);
+
+}
